7a7615129834c736e6eba20ca490ff897000a844
Akash Basu <abasu924@gmail.com>
2025-06-08 04:31:46 -0700

    :(

--- Assets/Scripts/MapGenerator/MapGenerator.cs
+++ Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
 
-
 namespace CMPM146.MapGenerator {
     public class MapGenerator : MonoBehaviour {
-        [Header ("Archetypes")]
+        [Header("Archetypes")]
         public RoomArchetype[] archetypes;
-        
-        [Header ("Generation Settings")]
+
+        [Header("Generation Settings")]
         public Hallway verticalHallway;
         public Hallway horizontalHallway;
 
@@ -20,47 +18,42 @@ namespace CMPM146.MapGenerator {
         public Room target;
 
         public int MIN_SIZE = 5;
-        
-        // Constraint: How big should the dungeon be at most
-        // this will limit the run time (~10 is a good value 
-        // during development, later you'll want to set it to 
-        // something a bit higher, like 25-30)
-        public int MAX_SIZE;
-
-        // set this to a high value when the generator works
-        // for debugging it can be helpful to test with few rooms
-        // and, say, a threshold of 100 iterations
+        public int MAX_SIZE = 30;
         public int THRESHOLD;
 
-        // keep the instantiated rooms and hallways here
         List<GameObject> _generatedObjects;
         List<Room> _generatedRooms;
         int _iterations;
-        
+
+        bool _targetPlaced;
+
         void Awake() {
             archetypes = Resources.LoadAll<RoomArchetype>("Rooms");
             List<RoomArchetype> a = archetypes.ToList();
             a.Remove(startRooms);
             archetypes = a.ToArray();
         }
-        
+
         public void Generate() {
-            // dispose of game objects from previous generation process
             foreach (GameObject go in _generatedObjects) {
                 Destroy(go);
             }
 
             _generatedObjects.Clear();
+            _generatedRooms = new List<Room>();
+            _targetPlaced = false;
 
             Room start = startRooms.GetRandomRoom();
-            // Markus's coordinate system can't handle anything outside Q1, this is a quick fix to that before I
-            // eventually fix that coordinate system.
             _generatedObjects.Add(start.Place(new Vector2Int(21, 21)));
-            List<Door>       doors    = start.GetDoors(new Vector2Int(21, 21));
+            _generatedRooms.Add(start);
+
+            List<Door> doors = start.GetDoors(new Vector2Int(21, 21));
             List<Vector2Int> occupied = new() { new Vector2Int(21, 21) };
+
             _iterations = 0;
             bool result = GenerateWithBacktracking(occupied, doors, 1);
-            if (!result) throw new Exception("Map Generation failed!");
+            if (!result || !_targetPlaced) throw new Exception("Map Generation failed or no target room placed!");
+
             PlaceHallways(_generatedRooms);
         }
 
@@ -69,47 +62,45 @@ namespace CMPM146.MapGenerator {
             _iterations++;
             if (doors.Count == 0) return depth >= MIN_SIZE;
 
-            Door       door       = doors[Random.Range(0, doors.Count)];
-            Door       match      = door.GetMatching();
-            Vector2Int offset     = match.GetGridCoordinates();
-            List<Room> validRooms = GetValidRooms(match.GetDirection());
-            
+            Door door = doors[Random.Range(0, doors.Count)];
+            Door match = door.GetMatching();
+            Vector2Int offset = match.GetGridCoordinates();
+
+            bool forceTarget = !_targetPlaced && depth >= MIN_SIZE;
+
+            List<Room> validRooms = GetValidRooms(match.GetDirection(), forceTarget);
             List<Door> newDoors = new(doors);
-            newDoors.RemoveSwapBack(door);
+            newDoors.Remove(door);
 
             while (validRooms.Count > 0) {
-                Room hopeful = RoomArchetype.GetRandomRoom(validRooms);
-                validRooms.RemoveSwapBack(hopeful);
+                int index = Random.Range(0, validRooms.Count);
+                Room hopeful = validRooms[index];
+                validRooms.RemoveAt(index);
+
                 List<Vector2Int> occMap = hopeful.GetOccupancy(match.GetGridCoordinates());
                 if (!CanFit(occMap, occupied)) continue;
-                // TODO: the above code needs to be revised to check each door on the side we're trying to place.
-                // this isn't a problem for 1x1 but will need adjustments for nxm
-                
-                // Attempt to place the room
-                // Add new doors to the door list. Remove the door mirror.
-                // TODO... adapt this to handle the case where a 2x2 room w/ 2 exits on same cardinal are being is
-                // being tested. Need to make sure we test for both upper & lower door placement.
+
                 List<Door> plusDoors = hopeful.GetDoors(offset);
-                // Heuristic to avoid exploring bad seeds too deeply. We assume each door leads to a new room.
-                // This overestimates slightly, this doesn't account for doors that connect to the same space.
-                // (placed rooms) + (frontier) > MAX_SIZE -- No, there isn't an off-by one error. 
                 if ((depth + plusDoors.Count + newDoors.Count) > MAX_SIZE) continue;
-                
-                plusDoors.RemoveSwapBack(match);
-                newDoors.AddRange(plusDoors);
-                
+
+                plusDoors.Remove(match);
+                List<Door> updatedDoors = new List<Door>(newDoors);
+                updatedDoors.AddRange(plusDoors);
+
                 List<Vector2Int> newOccupied = new(occupied);
                 newOccupied.AddRange(occMap);
-                
-                if (!GenerateWithBacktracking(newOccupied, newDoors, depth + 1)) continue;
-                // Then we're safe to place!
+
+                if (!GenerateWithBacktracking(newOccupied, updatedDoors, depth + 1)) continue;
+
                 GameObject obj = hopeful.Place(match.GetGridCoordinates());
                 _generatedObjects.Add(obj);
                 _generatedRooms.Add(obj.GetComponent<Room>());
-                // TODO: revisit this such that we place at the *correct* door. Some sort of offset may be needed. 
-                break;
+
+                if (hopeful == target) _targetPlaced = true;
+
+                return true;
             }
-            
+
             return false;
         }
 
@@ -126,14 +117,25 @@ namespace CMPM146.MapGenerator {
             }
             _generatedObjects.AddRange(hallways);
         }
-        
-        List<Room> GetValidRooms(in Door.Direction direction) {
+
+        List<Room> GetValidRooms(in Door.Direction direction, bool forceTarget) {
             List<Room> valid = new();
-            foreach (RoomArchetype archetype in archetypes) {
-                if (archetype.HasDoorOnSide(direction)) {
-                    valid.AddRange(archetype.GetAllRooms());
+
+            if (forceTarget) {
+                if (target.HasDoorOnSide(direction)) {
+                    valid.Add(target);
+                }
+            } else {
+                foreach (RoomArchetype archetype in archetypes) {
+                    if (archetype.HasDoorOnSide(direction)) {
+                        valid.AddRange(archetype.GetAllRooms());
+                    }
+                }
+                if (!_targetPlaced && target.HasDoorOnSide(direction)) {
+                    valid.Add(target);
                 }
             }
+
             return valid;
         }
 
@@ -141,7 +143,6 @@ namespace CMPM146.MapGenerator {
             foreach (Vector2Int occ in occMap) {
                 if (occupied.Contains(occ)) return false;
             }
-
             return true;
         }
 
